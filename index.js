94314636e9afb5a87ed3e1f240848878e031ffdc
“Erin <erinkelley27@gmail.com>
2018-09-10 15:48:40 -0400

    server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const parser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(parser.json())
+app.use(cors())
+
+app.listen(3001, () => console.log('listening on port 3001'))
