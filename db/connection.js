c1ad6c93465ac369c03833a52d86d92a1ee42b89
“Erin <erinkelley27@gmail.com>
2018-09-13 16:43:28 -0400

    heroku deployment adjustments

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = Promise
 
-mongoose.connect('mongodb://localhost/roadie')
-  .then(connection => console.log('connected!'))
-  .catch(err => console.log('connection failed!', err))
+if (process.env.NODE_ENV == 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/roadie')
+}
+
+// mongoose.connect('mongodb://localhost/roadie')
+//   .then(connection => console.log('connected!'))
+//   .catch(err => console.log('connection failed!', err))
 
 module.exports = mongoose
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express()
 const recController = require('./controllers/rec.js')
 const userController = require('./controllers/user.js')
 
+app.set('port', process.env.PORT || 3001)
 app.use(parser.json())
 app.use(cors())
 app.use(passport.initialize())
@@ -17,4 +18,11 @@ app.use(passport.initialize())
 app.use('/', recController)
 app.use('/user', userController)
 
-app.listen(3001, () => console.log('listening on port 3001'))
+// app.listen(3001, () => console.log('listening on port 3001'))
+// app.listen(app.get('port'), () => {
+//   console.log(`✅ PORT: ${app.get('port')} 🌟`)
+// })
+
+app.listen(app.get('port'), () => {
+  console.log('Server listening on port ' + app.get('port'))
+})
